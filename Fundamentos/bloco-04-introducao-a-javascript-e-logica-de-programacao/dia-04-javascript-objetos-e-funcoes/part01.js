35ebad7c5eb296e317a60a32477e4e14534f0160
Bruno Miranda Pereira <brunompe@gmail.com>
2021-08-02 18:10:42 -0400

    Parte 01 completa

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part01.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/part01.js
@@ -4,4 +4,34 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log ('Bem Vinda, ' + info.personagem);
+info ['recorrente'] = 'Sim';
+
+let infoMan = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+//console.log ('Bem Vinda, ' + info.personagem);
+
+info ['recorrente'] = 'Sim';
+
+//console.log (info);
+
+
+//for (let properties in info) {
+ /// console.log(info[properties]);
+//}
+
+//for (let properties in infoMan) {
+  //console.log(infoMan[properties]);
+//}
+
+  for (let properties in info) {
+    if (properties === 'recorrente' && info[properties]=== 'Sim' && infoMan[properties] === 'Sim') {
+      console.log ('Ambos recorrente');
+    } else {
+      console.log (info[properties] + ' e ' + infoMan[properties]);
+    }
+  }
